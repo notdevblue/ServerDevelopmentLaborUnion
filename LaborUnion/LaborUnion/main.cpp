68af6ee8766f3ca03284532663cdfbdeb52040af
DevblueNotWorking <wooyeophan@gmail.com>
2021-03-08 19:30:00 +0900

    깃헙에 있는 예시 코드 만약에 돌려볼것이라면 클라 코드는 없으니 PuTTY 깔아서 돌려봐요.

--- LaborUnion/LaborUnion/main.cpp
+++ LaborUnion/LaborUnion/main.cpp
@@ -3,9 +3,42 @@
 
 #pragma comment(lib, "ws2_32")
 
+#define PORT 5678
+#define PACKET_SIZE 1024
+
 int main()
 {
-	printf("와 깃허브 아시는구나\r\n");
+#pragma region 변수들
+	WSADATA		wsaData;
+	SOCKET		sListening;
+	SOCKET		sClient;
+	SOCKADDR_IN clientData;
+	SOCKADDR_IN serverData;
+	{
+		serverData.sin_addr.s_addr	= htonl(INADDR_ANY);
+		serverData.sin_port			= htons(PORT);
+		serverData.sin_family		= AF_INET;
+	}
+	INT			clientDataSize		= sizeof(clientData);
+	CHAR		chMsg[PACKET_SIZE] = "Connected.\r\nServer will shut down in 5 secs.";
+#pragma endregion
 
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	sListening = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	bind(sListening,  (SOCKADDR*)&serverData, sizeof(serverData));
+	listen(sListening, SOMAXCONN);
+	
+	sClient = accept(sListening, (SOCKADDR*)&clientData, &clientDataSize);
+	closesocket(sListening);
+
+	send(sClient, chMsg, PACKET_SIZE, 0);
+	Sleep(5000);
+
+	closesocket(sClient);
+	
+	WSACleanup();
 	return(0);
 }
