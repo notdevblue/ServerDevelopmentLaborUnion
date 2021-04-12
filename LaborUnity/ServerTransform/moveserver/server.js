7f4214a36ae2bdef7245e7ee897df72082ae77a2
Han not working these days <wooyeophan@gmail.com>
2021-04-12 15:25:28 +0900

    서버 기반만 만들어둠

---
+++ LaborUnity/ServerTransform/moveserver/server.js
@@ -0,0 +1,11 @@
+const WebSocket = require('ws');
+const port = 56789;
+const ws = new WebSocket.Server({ port }, () => {
+    console.log("서버 켜짐");
+});
+
+var msg = "Hello";
+
+ws.on("connection", socket => {
+    socket.send("hellomsg", msg);
+})
