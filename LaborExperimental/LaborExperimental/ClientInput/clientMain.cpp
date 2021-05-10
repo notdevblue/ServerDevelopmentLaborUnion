c514704392f21cc993d452bc5225705c1e09b8c5
Han Han Han <wooyeophan@gmail.com>
2021-05-10 16:05:00 +0900

    클라인풋 서버움직임  클라만 해둠  자야지 후후후

---
+++ LaborExperimental/LaborExperimental/ClientInput/clientMain.cpp
@@ -0,0 +1,32 @@
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <conio.h>
+#pragma comment(lib, "ws2_32")
+
+
+int main()
+{
+	WSADATA wsaData;
+	SOCKET clientSocket;
+	SOCKADDR_IN clientData;
+	{
+		clientData.sin_family = AF_INET;
+		clientData.sin_port = htons(56789);
+		InetPton(AF_INET, TEXT("127.0.0.1"), &clientData.sin_addr);
+	}
+	CHAR input[1];
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	
+	connect(clientSocket, (SOCKADDR*)&clientData, sizeof(clientData));
+
+	while (tolower(input[0] = _getch()) == 'q')
+	{
+		send(clientSocket, input, 1, 0);
+	}
+
+	
+	return(0);
+}
