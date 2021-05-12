0f33edeb50fdfa22f6262f899ea5bb004253453f
Han Han Han <wooyeophan@gmail.com>
2021-05-12 22:42:03 +0900

    공백 포함 입력과 렌더 로직만 수정하면 완성

--- LaborExperimental/LaborExperimental/ClientInput/clientMain.cpp
+++ LaborExperimental/LaborExperimental/ClientInput/clientMain.cpp
@@ -1,11 +1,37 @@
 #include <WS2tcpip.h>
+#include <Windows.h>
 #include <stdio.h>
 #include <conio.h>
 #pragma comment(lib, "ws2_32")
 
+bool g_isDisconnected = false;
+CRITICAL_SECTION crit;
+
+DWORD WINAPI recvThread(LPVOID lpSocket)
+{
+	printf("msg recv thread running...\r\n");
+	SOCKET socket = *(SOCKET*)lpSocket;
+
+	CHAR chMsg[1024];
+
+	COORD pos{ 0,3 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+
+	while (!g_isDisconnected)
+	{
+		recv(socket, chMsg, 1024, 0);
+		//system("cls");
+		printf("%s", chMsg);
+	}
+
+	return(0);
+}
 
 int main()
 {
+	InitializeCriticalSection(&crit);
+
+	HANDLE hRecv;
 	WSADATA wsaData;
 	SOCKET clientSocket;
 	SOCKADDR_IN clientData;
@@ -23,15 +49,22 @@ int main()
 	connect(clientSocket, (SOCKADDR*)&clientData, sizeof(clientData));
 	printf("connected.\r\n");
 	printf("q 를 눌러 나가세요: ");
+	hRecv = CreateThread(NULL, 0, recvThread, &clientSocket, 0, NULL);
 	while ((input[0] = tolower(input[0] = _getch())) != 'q')
 	{
 		send(clientSocket, input, 1, 0);
 	}
-
+	g_isDisconnected = true;
 
 	closesocket(clientSocket);
 
 	WSACleanup();
 	
+	if (WaitForSingleObject(hRecv, INFINITE) != WAIT_OBJECT_0)
+	{
+		printf("오 이런,");
+	}
+
+	DeleteCriticalSection(&crit);
 	return(0);
 }
--- LaborExperimental/LaborExperimental/ServerMove/serverMain.cpp
+++ LaborExperimental/LaborExperimental/ServerMove/serverMain.cpp
@@ -1,17 +1,39 @@
 #include <WS2tcpip.h>
+#include <Windows.h>
 #include <stdio.h>
 
 #pragma comment(lib, "ws2_32")
 
-CHAR chMsg[1];
-
 short g_posX = 0;
 short g_posY = 0;
+bool g_isDisconnected = false;
 
 void move(char input);
 
+DWORD WINAPI sendThread(LPVOID socket)
+{
+	SOCKET sendSocket = *(SOCKET*)socket;
+
+	COORD pos{ 10, 5 };
+	CHAR msg[1024];
+
+	while (!g_isDisconnected)
+	{
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+		//system("cls");
+		scanf_s("%[^\n]s", msg, 1024);
+		send(sendSocket, msg, 1024, 0);
+
+	}
+
+	return(0);
+}
+
 int main()
 {
+	
+	HANDLE hSend;
+
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
@@ -34,26 +56,37 @@ int main()
 	closesocket(sListening);
 	printf("client connected\r\n");
 
+	hSend = CreateThread(NULL, 0, sendThread, &sClient, 0, NULL);
+	CHAR chMsg[1];
 	while (true)
 	{
 		int buffer = recv(sClient, chMsg, 1, 0);
 		if (!buffer || buffer == -1)
+		{
 			break;
-
-		system("cls");
+		}
+		
 		move(chMsg[0]);
 	}
-
+	g_isDisconnected = true;
 
 	closesocket(sClient);
 	printf("\r\ndisconnected\r\n");
 	WSACleanup();
+
+	if (WaitForSingleObject(hSend, INFINITE) != WAIT_OBJECT_0)
+	{
+		printf("ø¿¿Ã∑±\r\n");
+	}
 
 	return(0);
 }
 
 void move(char input)
 {
+	int befX = g_posX;
+	int befY = g_posY;
+
 	switch (input)
 	{
 	case 'w':
@@ -82,8 +115,12 @@ void move(char input)
 		break;
 	}
 
-	COORD pos{ g_posX, g_posY };
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+
+	COORD befPos{ befX, befY };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), befPos);
+	printf(" ");
+	COORD newPos{ g_posX, g_posY };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), newPos);
 	printf("0");
 }
